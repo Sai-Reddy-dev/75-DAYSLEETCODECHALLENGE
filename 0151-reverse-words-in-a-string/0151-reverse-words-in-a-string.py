c3c21670839d777dae0fe32b5f81c5e733aa9459
velagala Durga Sai Gangireddy <153359680+Sai-Reddy-dev@users.noreply.github.com>
2026-05-10 11:42:37 +0530

    Time: 0 ms (100%), Space: 12.5 MB (68.27%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split()
+
+        reversed_words = words[::-1]
+        reversed_string = ' '.join(reversed_words)
+
+        return reversed_string
+        
