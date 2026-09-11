3b22cb27c900f274139dae234c82e9fe40dfa61c
velagala Durga Sai Gangireddy <153359680+Sai-Reddy-dev@users.noreply.github.com>
2026-09-11 12:37:22 +0530

    Time: 8 ms (31.35%), Space: 15.2 MB (5.89%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        result = 0
+        frequency = {}
+
         for num in nums:
-            result ^= num
-        return result
+            if num not in frequency:
+                frequency[num] = 1
+            else:
+                frequency[num] += 1
+        
+        for key,value in frequency.items():
+            if value == 1:
+                return key
+        return -1
+        
