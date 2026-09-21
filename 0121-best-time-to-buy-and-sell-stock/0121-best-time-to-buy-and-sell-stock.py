b05e4da9468e41412f020db1040aaa472d64b019
velagala Durga Sai Gangireddy <153359680+Sai-Reddy-dev@users.noreply.github.com>
2026-09-21 10:49:57 +0530

    Time: 24 ms (97.81%), Space: 19 MB (50.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -10,6 +10,6 @@ class Solution(object):
         for price in prices:
             if min_price > price:
                 min_price = price
-            else:
-                max_profit = max(max_profit,price-min_price)
+            elif price - min_price > max_profit:
+                max_profit = price-min_price
         return max_profit
